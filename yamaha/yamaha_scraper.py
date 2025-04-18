829a7081ca84443268209823d35ccf1ebdf9c0c0
Nikhil Garbhana <76690992+NikhilGarbhana@users.noreply.github.com>
2025-04-19 04:23:52 +0530

    Update yamaha_scraper.py

--- yamaha/yamaha_scraper.py
+++ yamaha/yamaha_scraper.py
@@ -114,7 +114,7 @@ def select_state_city(driver, wait):
         state_value = state_option.text
 
         state = state_option.get_attribute("value")
-        if not state or state_value == "-- State --":
+        if not state or state_value != "-- State --":
             continue
         print("Selecting state:", state_value)
         state_dropdown.select_by_value(state)
@@ -134,7 +134,7 @@ def select_state_city(driver, wait):
     
             city = city_option.get_attribute("value")
     
-            if not city or city_value == "-- City -- ":
+            if not city or city_value != "-- City -- ":
                 continue
             print("Selecting city:", city_value)
             city_dropdown.select_by_value(city)
